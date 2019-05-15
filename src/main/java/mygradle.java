106fb9eefdb8e92ce2ea819cbb5d93eb57f910a1
Premkumar <ppalanivel@blueally.com>
2019-05-15 17:54:07 +0530

    Initial commit

---
+++ src/main/java/mygradle.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+
+public class mygradle {
+    public static void main(String[] args) {
+        print(System.out);
+    }
+
+    public static void print(PrintStream out) {
+        out.println("Hello, Prem");
+    }
+}
